7afa1a922e6d0569f52b532f093ba47a733ed991
Quoc <phuquocfe@gmail.com>
2023-10-12 10:16:43 +0700

    lab5-7

--- lab1/main.js
+++ lab1/main.js
@@ -1,55 +1,55 @@
-let name1 = "Quoc";
-let tuoi1 = "20";
+// let name1 = "Quoc";
+// let tuoi1 = "20";
 
-const sayHello1 = () => {
-  console.log(`I'm ${name1}. I'm ${tuoi1}`)
-}
+// const sayHello1 = () => {
+//   console.log(`I'm ${name1}. I'm ${tuoi1}`)
+// }
 
-let name2 = "Nam";
-let tuoi2 = "23";
+// let name2 = "Nam";
+// let tuoi2 = "23";
 
-const sayHello2 = () => {
-  console.log(`I'm ${name2}. I'm ${tuoi2}`)
-}
-
-
+// const sayHello2 = () => {
+//   console.log(`I'm ${name2}. I'm ${tuoi2}`)
+// }
 
 
-const API = 'https://jsonplaceholder.typicode.com/';
-const ul = document.querySelector('ul');
 
-async function fetchData(endpoint) {
-  try {
-    const response = await fetch(`${API}${endpoint}`);
-    const data =  response.json();
-    return data;
-  } catch (error) {
-    console.error('Error:', error);
-    return [];
-  }
-}
 
-async function handleButtonClick(event) {
+// const API = 'https://jsonplaceholder.typicode.com/';
+// const ul = document.querySelector('ul');
 
-  sayHello1();
-  sayHello2();
+// async function fetchData(endpoint) {
+//   try {
+//     const response = await fetch(`${API}${endpoint}`);
+//     const data =  response.json();
+//     return data;
+//   } catch (error) {
+//     console.error('Error:', error);
+//     return [];
+//   }
+// }
 
-  const { dataset } = event.target;
-  const { endpoint } = dataset;
+// async function handleButtonClick(event) {
 
-  try {
-    const data = await fetchData(endpoint);
+//   sayHello1();
+//   sayHello2();
 
-    data.forEach(item => {
-      const { title } = item;
-      const li = `<li>${title}</li>`;
-      ul.innerHTML += li;
-    });
-  } catch (error) {
-    console.error('Error:', error);
-  }
-}
+//   const { dataset } = event.target;
+//   const { endpoint } = dataset;
 
-const btn = document.querySelector('button');
-btn.addEventListener('click', handleButtonClick);
+//   try {
+//     const data = await fetchData(endpoint);
+
+//     data.forEach(item => {
+//       const { title } = item;
+//       const li = `<li>${title}</li>`;
+//       ul.innerHTML += li;
+//     });
+//   } catch (error) {
+//     console.error('Error:', error);
+//   }
+// }
+
+// const btn = document.querySelector('button');
+// btn.addEventListener('click', handleButtonClick);
 
--- lab2/lession2/main.js
+++ lab2/lession2/main.js
@@ -1,7 +1,7 @@
 const source = [1,2,3,4,5,6,7,8,9,10];
 
 function removeFirstTwo (list) {
-  const [a,b, rest] = list
+  const [a,b, ...rest] = list
 
   return rest 
 }
